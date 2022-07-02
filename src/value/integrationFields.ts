36dda0edc79aea9fb245c78e136e54187665ae7f
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-07-01 15:09:00 -1000

    fix: use corrected IntegrationFields value

--- src/value/integrationFields.ts
+++ src/value/integrationFields.ts
@@ -1,46 +1,29 @@
 import * as prismicT from "@prismicio/types";
-import * as changeCase from "change-case";
-
-import { createFaker } from "../lib/createFaker";
-import { getMockImageData } from "../lib/getMockImageData";
 
 import { MockValueStateConfig, MockValueConfig } from "../types";
 
 export type MockIntegrationFieldsValueConfig<
 	Model extends prismicT.CustomTypeModelIntegrationFieldsField = prismicT.CustomTypeModelIntegrationFieldsField,
-	Blob = unknown,
+	Data extends Record<string, unknown> = Record<string, unknown>,
 	State extends prismicT.FieldState = prismicT.FieldState,
 > = {
-	data?: Blob;
+	data?: Data;
 } & MockValueConfig<Model> &
 	MockValueStateConfig<State>;
 
 export type MockIntegrationFieldsValue<
-	Blob = unknown,
+	Data extends Record<string, unknown> = Record<string, unknown>,
 	State extends prismicT.FieldState = prismicT.FieldState,
-> = prismicT.IntegrationFields<Blob, State>;
+> = prismicT.IntegrationFields<Data, State>;
 
 export const integrationFields = <
 	Model extends prismicT.CustomTypeModelIntegrationFieldsField = prismicT.CustomTypeModelIntegrationFieldsField,
-	Blob = unknown,
+	Data extends Record<string, unknown> = Record<string, unknown>,
 	State extends prismicT.FieldState = "filled",
 >(
-	config: MockIntegrationFieldsValueConfig<Model, Blob, State> = {},
-): MockIntegrationFieldsValue<Blob, State> => {
-	const faker = createFaker(config.seed);
-
-	const imageData = getMockImageData({ seed: config.seed });
-
+	config: MockIntegrationFieldsValueConfig<Model, Data, State> = {},
+): MockIntegrationFieldsValue<Data, State> => {
 	return (
-		config.state === "empty"
-			? null
-			: {
-					id: faker.hash(7),
-					title: changeCase.capitalCase(faker.words(faker.range(1, 3))),
-					description: changeCase.sentenceCase(faker.words(faker.range(5, 10))),
-					image_url: imageData.url,
-					last_update: faker.date().getTime(),
-					blob: config.data as Blob,
-			  }
-	) as MockIntegrationFieldsValue<Blob, State>;
+		config.state === "empty" ? null : config.data
+	) as MockIntegrationFieldsValue<Data, State>;
 };
