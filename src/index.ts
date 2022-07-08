96ee969859dec0e34499b4952f28116f0e0593bd
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-07-07 16:11:33 -1000

    fix: resolve bundle order bug where `customType` was used before it was defined

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
+export * as api from "./api";
+export * as model from "./model";
+export * as value from "./value";
+
 export { createMockFactory, MockFactory } from "./createMockFactory";
 
 export {
 	createAPIMockFactory,
 	APIMockFactory,
 } from "./api/createAPIMockFactory";
-export * as api from "./api";
-
 export {
 	createModelMockFactory,
 	ModelMockFactory,
 } from "./model/createModelMockFactory";
-export * as model from "./model";
-
 export {
 	createValueMockFactory,
 	ValueMockFactory,
 } from "./value/createValueMockFactory";
-export * as value from "./value";
