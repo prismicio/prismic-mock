95ab9dd333e101e0ad2ae1747959e75f4ff749ea
Daniel Martín <danimartinprieto@gmail.com>
2024-09-20 13:29:33 +0200

    Fix whitespace

--- src/value/contentRelationship.ts
+++ src/value/contentRelationship.ts
@@ -4,13 +4,17 @@ import { buildContentRelationshipField } from "../lib/buildContentRelationshipFi
 import { createFaker } from "../lib/createFaker";
 import { generateCustomTypeId } from "../lib/generateCustomTypeId";
 import { generateTags } from "../lib/generateTags";
+
 import {
 	MockValueStateConfig,
 	MockValueConfig,
 	IterableElement,
 } from "../types";
+
 import * as modelGen from "../model";
+
 import { document as documentGen } from "./document";
+
 export type MockContentRelationshipValueConfig<
 	Model extends
 		prismic.CustomTypeModelContentRelationshipField = prismic.CustomTypeModelContentRelationshipField,
@@ -22,7 +26,8 @@ export type MockContentRelationshipValueConfig<
 	linkableDocuments?: prismic.PrismicDocument[];
 } & MockValueConfig<Model> &
 	MockValueStateConfig<State>;
-type MockContentRelationshipValue<
+
+	type MockContentRelationshipValue<
 	Model extends
 		prismic.CustomTypeModelContentRelationshipField = prismic.CustomTypeModelContentRelationshipField,
 	State extends prismic.FieldState = prismic.FieldState,
@@ -32,6 +37,7 @@ type MockContentRelationshipValue<
 	never,
 	State
 >;
+
 export const contentRelationship = <
 	Model extends
 		prismic.CustomTypeModelContentRelationshipField = prismic.CustomTypeModelContentRelationshipField,
@@ -40,12 +46,14 @@ export const contentRelationship = <
 	config: MockContentRelationshipValueConfig<Model, State>,
 ): MockContentRelationshipValue<Model, State> => {
 	const faker = config.faker || createFaker(config.seed);
+
 	if (config.state === "empty") {
 		return {
 			link_type: prismic.LinkType.Document,
 		} as MockContentRelationshipValue<Model, State>;
 	} else {
 		const model = config.model || modelGen.contentRelationship({ faker });
+
 		const linkableDocuments = config.linkableDocuments
 			? config.linkableDocuments.filter(
 					(
@@ -55,10 +63,12 @@ export const contentRelationship = <
 						NonNullable<NonNullable<Model["config"]>["customtypes"]>[number]
 					> => {
 						let shouldKeep = true;
+
 						if (model.config?.customtypes) {
 							shouldKeep =
 								shouldKeep && model.config.customtypes.includes(document.type);
 						}
+
 						if (model.config?.tags) {
 							shouldKeep =
 								shouldKeep &&
@@ -82,7 +92,9 @@ export const contentRelationship = <
 						NonNullable<NonNullable<Model["config"]>["customtypes"]>[number]
 					>,
 				];
-		const document = faker.randomElement(linkableDocuments);
+
+				const document = faker.randomElement(linkableDocuments);
+
 		if (!document) {
 			throw new Error("A linkable document could not be found.");
 		}
